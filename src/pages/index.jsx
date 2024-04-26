048f912b56f0eacfeb9256233e47bf5503f4094b
John X <john.iv@x25bd.com>
2024-04-26 17:14:23 +0100

    Adding friends' logos

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,7 @@ const Home = () => (
     <Intro />
     <Features />
     <h3 id="features-footer">Now, smart contracts are connected to the <strong>real world</strong>.</h3>
+    <Supporters />
 
     <MaxWidth id="work" className="Home__projects">
       {Object.entries(Projects).map(([key, value], index) => (
@@ -24,7 +25,6 @@ const Home = () => (
         />
       ))}
     </MaxWidth>
-    <Supporters />
   </Layout>
 )
 
