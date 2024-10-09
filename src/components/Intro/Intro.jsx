488c8f9f3c9e03f381079ea81fc140040c8886e0
John X <john.iv@x25bd.com>
2024-10-09 16:36:37 +0100

    Optimizing mobile header buttons

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import {useState, useEffect, React} from "react"
 import classNames from "classnames"
 import Link from "components/_ui/Link/Link"
 import MaxWidth from "components/_ui/MaxWidth/MaxWidth"
@@ -7,6 +7,24 @@ import { faDiscord } from "@fortawesome/free-brands-svg-icons"
 import "./Intro.scss"
 
 const Intro = () => {
+
+  const [windowWidth, setWindowWidth] = useState(typeof window !== 'undefined' ? window.innerWidth : 0);
+
+  useEffect(() => {
+      // Function to update state based on window width
+      const handleResize = () => {
+          setWindowWidth(window.innerWidth);
+      };
+
+      // Set up event listener for window resize
+      window.addEventListener('resize', handleResize);
+
+      // Clean up the event listener when the component unmounts
+      return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+
+
   return (
     <MaxWidth size="m" className="Intro">
       <h1 className="Intro__header">
@@ -14,6 +32,19 @@ const Intro = () => {
       </h1>
       <p class="sub-heading">Astral develops open source tools + infrastructure <br />so you can easily build location-based dapps. </p>
       <div className="Intro__buttons">
+        { windowWidth <= 1000 ? (
+          <Link
+          doOpenInNewTab
+          isButton
+          className="Intro__buttons_button"
+          buttonProps={{ category: "filled" }}
+          // TODO: restore back to subdomain once issue is closed (https://github.com/AstralProtocol/astralprotocol/issues/20#issue-2026525326).
+          //
+          // to="https://docs.astral.global" 
+          to="https://logbook.astral.global"
+        >
+          Logbook App →
+        </Link>) : null }
         <Link
           doOpenInNewTab
           isButton
@@ -26,6 +57,16 @@ const Intro = () => {
         >
           Read the docs
         </Link>
+        { windowWidth <= 1000 ? (
+          <Link
+          doOpenInNewTab
+          isButton
+          className="Intro__buttons_button"
+          buttonProps={{ category: "outlined" }}
+          to="https://dorahacks.io/hackathon/ftc-2024/tracks#environmental-impact-by-klimadao"
+        >
+          Enter the FtC x EC Hackathon ↗
+        </Link>) : null }
         <Link
           doOpenInNewTab
           isButton
